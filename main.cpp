a444244ad670405da4c59908f6df925ab4255b2d
KrisS0n <gila77591@gmail.com>
2025-01-03 23:51:43 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <limits>
+
+int main() {
+    int number;
+
+    while (true) {
+        std::cout << "Enter an integer: ";
+
+        try {
+            std::cin >> number;
+
+            if (!std::cin) {
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                throw std::runtime_error("Incorrect input! Please enter a number, not a letter.");
+            }
+
+            if (std::cin.peek() != '\n') {
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                throw std::runtime_error("Incorrect input! Please enter a number without a floating point or space.");
+            }
+
+            if (number <= 0) {
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                throw std::runtime_error("Incorrect input! Please enter a positive number.");
+            }
+
+            break;
+
+        } catch (const std::runtime_error& ex) {
+            std::cerr << ex.what() << std::endl;
+        }
+    }
+
+    std::cout << "You have entered a number: " << number << std::endl;
+
+    return 0;
+}
